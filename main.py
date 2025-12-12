2d1086884f97cf5f8bedf2379a3840229651d4c3
Morgangx <morgangbulawa@gmail.com>
2025-12-12 19:56:23 +0100

    finished the project

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from stats import get_book_text, get_number_of_words
+from stats import get_book_text, get_number_of_words, count_characters, get_characters, CharCount
+import sys
+
 
 def main() -> None:
-    contents: str = get_book_text("books/frankenstein.txt")
+    print(sys.argv)
+    print("Usage: python3 main.py <path_to_book>")
+    if len(sys.argv) != 2: sys.exit(1)
+    book_path: str = sys.argv[1]
+    
+    contents: str = get_book_text(book_path)
     word_count: int = get_number_of_words(contents)
-    print(f"Found {word_count} total words")
+    char_dict: dict[str, int] = count_characters(contents)
+    sorted_chars: list[CharCount] = get_characters(char_dict)
 
+    characters_listings: str = ""
+    for char_count in sorted_chars:
+        if char_count["char"].isalpha():
+            characters_listings += f"{char_count["char"]}: {char_count["num"]}\n"
+    print(
+        f"============ BOOKBOT ============\n"+
+        f"Analyzing book found at {book_path}...\n"+
+        "----------- Word Count ----------]\n"+
+        f"Found {word_count} total words\n"+
+        "--------- Character Count -------\n"+
+        f"{characters_listings}"+
+        "============= END ==============="
+    )
+    
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,9 @@
+from typing import TypedDict
+
+class CharCount(TypedDict):
+    char: str
+    num: int
+
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         file_contents: str = f.read()
@@ -6,3 +12,27 @@ def get_book_text(filepath: str) -> str:
 def get_number_of_words(contents: str) -> int:
     words: list[str] = contents.split()
     return len(words)
+
+def count_characters(contents: str) -> dict[str, int]:
+    char_dict: dict[str, int] = {}
+    contents_l = contents.lower()
+    for ch in contents_l:
+        if ch not in char_dict:
+            char_count: int = contents_l.count(ch)
+            char_dict[ch] = char_count
+    return char_dict
+
+def get_characters(character_counts: dict[str, int]) -> list[CharCount]:
+    list_of_characters: list[CharCount] = []
+    for k in character_counts:
+        list_of_characters.append({
+            "char": k,
+            "num": character_counts[k]
+            })
+
+    def sort_by_num(dictionary: CharCount) -> int:
+        return dictionary["num"]
+
+    list_of_characters.sort(reverse=True, key=sort_by_num)
+
+    return list_of_characters
